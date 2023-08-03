3e6a54e874ec4ab01b78c6cd6b76bbbc074931ab
shinyfuwante <mr.philliptang@gmail.com>
2023-08-02 21:05:34 -0700

    Set up front end to communicate to back end

--- frontend/url-shortener/src/App.tsx
+++ frontend/url-shortener/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+export type shortUrl = {
+  id: number;
+  short_url: string;
+  full_url: string;
+  num_clicked: number;
+  description: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const backendUrl = "http://localhost:8080/";
+  const [shortUrls, setShortUrls] = useState<shortUrl[]>();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const fetchAllShortUrls = async () => {
+    const response = await fetch(backendUrl + "short_urls/");
+    const json = await response.json();
+    setShortUrls(await json.data);
+  };
+  useEffect(() => {
+    fetchAllShortUrls();
+  }, []);
+  return <>hi</>;
 }
 
-export default App
+export default App;
