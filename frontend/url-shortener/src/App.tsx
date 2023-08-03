4c6b338502f1a2b9c60b04250dbe8f91f94ab39b
shinyfuwante <mr.philliptang@gmail.com>
2023-08-02 21:21:48 -0700

    Testing

--- frontend/url-shortener/src/App.tsx
+++ frontend/url-shortener/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
+import nanoid;
 import "./App.css";
 
 export type shortUrl = {
   id: number;
-  short_url: string;
+  short: string;
   full_url: string;
   num_clicked: number;
   description: string;
@@ -11,17 +12,32 @@ export type shortUrl = {
 
 function App() {
   const backendUrl = "http://localhost:8080/";
-  const [shortUrls, setShortUrls] = useState<shortUrl[]>();
+  const [shortUrls, setShortUrls] = useState<shortUrl[]>([]);
 
   const fetchAllShortUrls = async () => {
     const response = await fetch(backendUrl + "short_urls/");
     const json = await response.json();
-    setShortUrls(await json.data);
+    setShortUrls(await json.data.data);
   };
   useEffect(() => {
     fetchAllShortUrls();
   }, []);
-  return <>hi</>;
+
+  const topFiveShorts = () => {
+    return (
+      <>
+        {shortUrls.map((short) => (
+          <div key={short.id}>
+            {short.short}
+            -----------------
+            {short.full_url}
+            *---------------*
+          </div>
+        ))}
+      </>
+    );
+  };
+  return <>{shortUrls.length > 0 ? topFiveShorts() : <>{shortUrls.length}</>}</>;
 }
 
 export default App;
