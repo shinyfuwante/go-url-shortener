ec20af234a72b5ef57b995fe74be6a52480b4bcf
shinyfuwante <mr.philliptang@gmail.com>
2023-08-08 21:02:45 -0700

    Redirect on link click

--- frontend/url-shortener/src/components/TopShortUrls/TopShortUrls.tsx
+++ frontend/url-shortener/src/components/TopShortUrls/TopShortUrls.tsx
@@ -7,24 +7,34 @@ interface TopShortUrlsProps {
 }
 
 export const TopShortUrls: React.FC<TopShortUrlsProps> = ({ shortUrls }) => {
-    const backendUrl = "localhost:8080/"
+  const backendUrl = "http://localhost:8080";
   return (
     <table className="top-short-url-container">
-      <tr>
-        <th colSpan="3" className="top-short-url-title">Top Urls</th>
-      </tr>
-      <tr>
-        <th>Short:</th>
-        <th>Full Url:</th>
-        <th>Times visited:</th>
-      </tr>
-      {shortUrls.map((short) => (
-        <tr key={short.id}>
-          <td><a href={backendUrl+short.short}>{short.short}</a></td>
-          <td><a href={short.full_url}>{short.full_url}</a></td>
-          <td>{short.num_clicked}</td>
+      <thead>
+        <tr>
+          <th colSpan="3" className="top-short-url-title">
+            Top Urls
+          </th>
         </tr>
-      ))}
+        <tr>
+          <th>Short:</th>
+          <th>Full Url:</th>
+          <th>Times visited:</th>
+        </tr>
+      </thead>
+      <tbody>
+        {shortUrls.map((short) => (
+          <tr key={short.id}>
+            <td>
+              <a href={`${backendUrl}/${short.short}`}>{short.short}</a>
+            </td>
+            <td>
+              <a href={short.full_url}>{short.full_url}</a>
+            </td>
+            <td>{short.num_clicked}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
