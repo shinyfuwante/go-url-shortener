9e4d3194a84ba78c97d8ae58e885120c595350d0
shinyfuwante <mr.philliptang@gmail.com>
2023-09-24 20:21:18 -0700

    Clean up

--- configs/setup.go
+++ configs/setup.go
@@ -19,11 +19,6 @@ func ConnectToMongo() *mongo.Client {
 		panic(err)
 	}
 
-	// defer func() {
-	// 	if err = client.Disconnect(context.TODO()); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
 	// Send a ping to confirm a successful connection
 	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
 		panic(err)
