48ef3660c6291bcb6fa31e204bb50bcadbf99343
shinyfuwante <mr.philliptang@gmail.com>
2023-09-26 20:44:24 -0700

    Host on 0.0.0.0

--- frontend/url-shortener/vite.config.ts
+++ frontend/url-shortener/vite.config.ts
@@ -5,6 +5,7 @@ import 'dotenv/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
     port: <number><unknown>process.env.PORT,
   },
   plugins: [react()],
