6f07e7e830a474cd0990962df7115ec6f0f0df6e
shinyfuwante <mr.philliptang@gmail.com>
2023-09-26 20:16:15 -0700

    Some more configs for railway

--- frontend/url-shortener/vite.config.ts
+++ frontend/url-shortener/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import 'dotenv/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: <number><unknown>process.env.PORT,
+  },
   plugins: [react()],
 })
