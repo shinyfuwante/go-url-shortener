f3c9d17b426521109e32cffe01a9c20f6a15417f
shinyfuwante <mr.philliptang@gmail.com>
2023-09-27 22:00:51 -0700

    Fix some cors config

--- backend/configs/cors.go
+++ backend/configs/cors.go
@@ -6,7 +6,6 @@ import (
 
 func ConfigCors() cors.Config {
 	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
 	config.AllowOrigins = []string{"https://metamon.dev/"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Authorization", "Content-Type"}
