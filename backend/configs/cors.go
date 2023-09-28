64ffd3487893e2b416456487b365cf497f3c6222
shinyfuwante <mr.philliptang@gmail.com>
2023-09-27 21:56:59 -0700

    Fix back end to only allow traffic from front end

--- backend/configs/cors.go
+++ backend/configs/cors.go
@@ -7,7 +7,7 @@ import (
 func ConfigCors() cors.Config {
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
-	// config.AllowOrigins = []string{"http://localhost:5173"}
+	config.AllowOrigins = []string{"https://metamon.dev/"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Authorization", "Content-Type"}
 
